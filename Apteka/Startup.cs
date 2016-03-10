d30668d290fb2ba1b1b74ba66213cfe3ba982209
Przemyslaw Banasiak <200299@student.pwr.wroc.pl>
2016-03-10 09:21:51 +0100

    Create Admin user on startup

--- Apteka/Startup.cs
+++ Apteka/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using Apteka.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Apteka.Startup))]
@@ -9,6 +12,29 @@ namespace Apteka
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesAndUsers();
+        }
+
+        private void createRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            foreach (var role in AppConfig.defaultRoles) {
+                if (!roleManager.RoleExists(role)) {
+                    roleManager.Create(new IdentityRole { Name = role });
+                }
+            }
+            
+            if (UserManager.FindByName(AppConfig.defaultUser.UserName) == null) {
+                var user = new ApplicationUser { UserName = AppConfig.defaultUser.UserName, Email = AppConfig.defaultUser.Email };
+                var chkUser = UserManager.Create(user, AppConfig.defaultUser.Password);
+                if (chkUser.Succeeded) {
+                    var result1 = UserManager.AddToRole(user.Id, AppConfig.defaultUser.Role);
+                }
+            }
         }
     }
 }
