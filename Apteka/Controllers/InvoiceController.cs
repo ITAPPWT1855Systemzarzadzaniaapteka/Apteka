37505e3a085f57d071ea0d431f57b6c59f3d4a80
banasiak <przemyslaw.banasiak@nokia.com>
2016-05-09 14:47:53 +0200

    Fix invoice create and chart on cleanup home

--- Apteka/Controllers/InvoiceController.cs
+++ Apteka/Controllers/InvoiceController.cs
@@ -36,23 +36,10 @@ namespace Apteka.Controllers
         [HttpGet]
         public ActionResult GetWarehouses()
         {
-            var warehouses = context.Hurtownia.ToList();
-            List<JsonWarehouse> misioweHurtownie = new List<JsonWarehouse>();
-
-            foreach (var warehouse in warehouses)
-            {
-                var misiu = new JsonWarehouse();
-                misiu.label = warehouse.Nazwa;
-                misiu.value = warehouse.ID_hurtownia;
-                misioweHurtownie.Add(misiu);
-            }
-           
-            return new JsonResult
-            {
-                Data = misioweHurtownie,
-                JsonRequestBehavior = JsonRequestBehavior.AllowGet
-                
-            };
+            var warehouses = context.Hurtownia.ToList()
+                .Select(i => new { i.Id_hurtownia, i.Nazwa, i.NIP })
+                .ToList();
+            return Json(warehouses, JsonRequestBehavior.AllowGet);
         }
 
         [HttpGet]
